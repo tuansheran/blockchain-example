810116600992d31ada3f3459fbaa4935faba34a8
sheranbuckmangit <sherantuanbuckman@gmail.com>
2024-05-12 11:12:34 +0530

    feat(): implement tsconfig file, Transaction & Block classes

---
+++ index.ts
@@ -0,0 +1,31 @@
+import * as crypto from 'crypto';
+
+class Transaction {
+    constructor(
+        public amount: number,
+        public payer: string,
+        public payee: string,
+    ){}
+
+    toString(){
+        return JSON.stringify(this)
+    }
+}
+
+
+class Block {
+    constructor(
+        public prevHash: string,
+        public transaction: Transaction,
+        public ts = Date.now()
+    ){
+
+    }
+
+    get hash(){
+        const str = JSON.stringify(this);
+        const hash = crypto.createHash('SHA256');
+        hash.update(str).end();
+        return hash.digest('hex');
+    }
+}
